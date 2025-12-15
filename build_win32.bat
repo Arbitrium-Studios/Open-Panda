bf0a54ca3976126a4b0dcaa78093221b437cdc87
Gracie Lovelight <gracielovelight@gmail.com>
2025-12-14 22:23:33 -0500

    Updated build_win32.bat
    Updated build_win32.bat to dynamically switch between thirdparty tools depending on the system version as well as allowing the user to set a custom Visual C++ version and Windows SDK version including a fallback to "14.3" for Visual C++ and "11" for the Windows SDK. On top of this, I added ".7z" files to the ".gitignore"

--- build_win32.bat
+++ build_win32.bat
@@ -1,45 +1,63 @@
 @echo off
-title Open-Panda Builder
+title Nexus Applications' Open-Panda3D Builder
+
 if not exist thirdparty/win-nsis (
+
+    set PROGRAM_FILES_X86="%ProgramFiles(x86)%"
+
     echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     echo Fetching ThirdParty Dependencies
     echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-    if not exist panda3d-1.10.15-tools-win64.zip ( 
-        "thirdparty\win-wget\wget" https://www.panda3d.org/download/panda3d-1.10.15/panda3d-1.10.15-tools-win64.zip
+
+    if not exist "%PROGRAM_FILES_X86%" (
+        set PANDA_THIRDPARTY_TOOLS_FILE = panda3d-1.10.15-tools-win32.zip
+
+        echo.
+        echo System is 32-bit, setting ThirdParty Tools accordingly
+        echo.
     )
-    if not exist panda3d-1.10.15 ( 
-        "thirdparty\win-7zip\7z" x panda3d-1.10.9-tools-win64.zip
+
+    if exist "%PROGRAM_FILES_X86%" (
+        set PANDA_THIRDPARTY_TOOLS_FILE = panda3d-1.10.15-tools-win64.zip
+
+        echo.
+        echo System is 64-bit, setting ThirdParty Tools accordingly
+        echo.
     )
+
+    if not exist "%PANDA_THIRDPARTY_TOOLS_FILE%" (
+        "thirdparty\win-wget\wget" https://www.panda3d.org/download/panda3d-1.10.15/%PANDA_THIRDPARTY_TOOLS_FILE%
+    )
+
+    if not exist panda3d-1.10.15 (
+        "thirdparty\win-7zip\7z" x %PANDA_THIRDPARTY_TOOLS_FILE%
+    )
+
     robocopy "panda3d-1.10.15\thirdparty" "thirdparty" /E /NFL /MOVE
     rmdir /q panda3d-1.10.15
 )
 
 echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-echo Building Panda3D
+echo Building Nexus Applications' Open-Panda3D
 echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-set /P THREADS="Thread Count: "
-"thirdparty\win-python3.13-x64\python" makepanda/makepanda.py --everything --installer --msvc-version=14.3 --windows-sdk=11 --no-eigen --threads=%THREADS%
-PAUSE
 
-@echo off
-title Open-Panda Builder
-if not exist thirdparty/win-nsis (
-    echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-    echo Fetching ThirdParty Dependencies
-    echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-    if not exist panda3d-1.10.15-tools-win64.zip ( 
-        "thirdparty\win-wget\wget" https://www.panda3d.org/download/panda3d-1.10.15/panda3d-1.10.15-tools-win64.zip
-    )
-    if not exist panda3d-1.10.15 ( 
-        "thirdparty\win-7zip\7z" x panda3d-1.10.15-tools-win64.zip
-    )
-    robocopy "panda3d-1.10.15\thirdparty" "thirdparty" /E /NFL /MOVE
-    rmdir /q panda3d-1.11.2
-)
+set "MSVC_VERSION=14.3"
+set /P "MSVC_VERSION=Microsoft Visual C++ Version (i.e. 2022 is 14.3) [%MSVC_VERSION%]: "
 
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-echo Building Panda3D
-echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+echo.
+echo Microsoft Visual Studio Version is set to %MSVC_VERSION%
+
+set "WIN_SDK=11"
+set /P WIN_SDK="Windows SDK Version (i.e. 11 for Windows 11): "
+
+echo.
+echo Windows SDK Version is set to Windows 11
+
 set /P THREADS="Thread Count: "
-"thirdparty\win-python3.13-x64\python" makepanda/makepanda.py --everything --installer --msvc-version=14.3 --windows-sdk=11 --no-eigen --threads=%THREADS%
-PAUSE
+
+echo.
+echo Thread Count is set to %THREADS%
+
+"thirdparty\win-python3.13-x64\python" makepanda/makepanda.py --everything --installer --msvc-version=%MSVC_VERSION% --windows-sdk=%WIN_SDK% --no-eigen --threads=%THREADS%
+
+pause
